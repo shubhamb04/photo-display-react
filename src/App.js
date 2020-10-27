7bde9a555c8cd845c3a3c954cbd8461125e9b026
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2020-10-27 19:41:31 +1100

    gave header to app file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Hi you are on main page</h2>
+    </div>
+  );
 }
 
 export default App;
